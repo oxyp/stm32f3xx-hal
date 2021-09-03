97100b54fa2a71cdbdd0b71a9ad98cff2358dd80
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-09-03 12:49:41 +0200

    at least use right CCM_RAM boundary

--- examples/flash_write.rs
+++ examples/flash_write.rs
@@ -7,20 +7,24 @@ use defmt_rtt as _; // global logger
 use panic_semihosting as _;
 use stm32f3xx_hal::{pac, prelude::*};
 
-// stolen from another stm32f3 example memory.x, I hope this lines up
-const RAM_START: u32 = 0x20000000;
+const CCM_RAM_START: u32 = 0x10000000;
 const PAGE_SZE: u32 = 0x7d0; // 2 KB
 
 #[entry]
 /// Main Thread
 fn main() -> ! {
-    let test_address = RAM_START - PAGE_SZE;
-    // Get peripherals, clocks and freeze them
+    // TODO make sure that this points to a page *start*?
+
+    let test_address = 268433000; //CCM_RAM_START - PAGE_SZE;
+
     let dp = pac::Peripherals::take().unwrap();
 
-    defmt::error!("erasing flash page at {}...", test_address);
+    defmt::info!("erasing flash page at {:x}...", test_address);
     let erase_result = dp.FLASH.page_erase(test_address);
     defmt::info!("erase result: {}", erase_result);
+
+    // TODO try out write when erase works
+    //let erase_result = dp.FLASH.page_write(test_address);
 
     // make sure function is diverging
     loop {
